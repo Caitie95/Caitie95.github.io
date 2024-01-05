d307375c9c19797e84115a93ed7be7186470facf
Caitie95 <135362135+Caitie95@users.noreply.github.com>
2024-01-05 17:48:56 +0000

    more styling

--- src/app/data/story-data.ts
+++ src/app/data/story-data.ts
@@ -5,7 +5,7 @@ export const STORIES: DataStory[] = [
         description: "Boom", warnings: [1], link: "https://clarkesworldmagazine.com/", source: 1},
     { id: 2, title: "Test Title 2", author: 2, genres: [2], identities: [{romantic: 2, sexuality: 2, explicit: 2, prominance: 2}],
         description: "Yaaaaay!", warnings: [2]},
-    { id: 3, title: "I Am A Very Good Title, Please Love Me", author: 2, genres: [1], 
+    { id: 3, title: "I Am A Very Good Title, Please Love Me", author: 2, genres: [1, 2], 
         identities: [{romantic: 2, sexuality: 1, explicit: 2, prominance: 1}, {romantic: 1, sexuality: 1, explicit: 1, prominance: 2}],
         warnings: [1], description: "A short story about a story that wants love."}
 ]
